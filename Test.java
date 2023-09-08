7af41672b48f74285e1b814cfa85d244bf616c3e
4nguru <4nguru@gmail.com>
2023-09-08 09:33:51 +0530

    Add files via upload

--- Test.java
+++ Test.java
@@ -1,1 +1,14 @@
+public class While {
+       public static void main(String[] args)
+       {
+          int i = 0;
+          System.out.println("While loop");
+          while ( i < 5 ) {
+              System.out.println(i);
+              i++;
+          }
 
+          System.out.println("For loop");
+          for(i = 5;i>0; i--) System.out.println(i);
+       }
+}
